d69c31a6533a47fcecae19c249bc69807e216b61
Jon Flash <funkswing@gmail.com>
2015-06-30 12:34:22 -0400

    Tornado Test

---
+++ tornado_main.py
@@ -0,0 +1,37 @@
+__author__ = 'jflaisha'
+
+import motor
+from tornado import gen
+from tornado.ioloop import IOLoop
+from tornado.web import RequestHandler, Application, url, asynchronous
+
+
+class TestHandler(RequestHandler):
+    @asynchronous
+    @gen.coroutine
+    def get(self):
+        db = self.settings['db']
+        document = yield db.sam.find_one([('run_type', "single")])
+        self.write(document)
+        print document
+
+
+def make_app():
+    db = motor.MotorClient().ubertool   # Create single DB instance, and pass that to the Application
+    return Application([
+        url(r"/", TestHandler),
+        ], db)
+
+
+def main():
+    app = make_app()
+    app.listen(8787)
+    # server = tornado.httpserver.HTTPServer(app)
+    # server.bind(8888)
+    # server.start(0)  # forks one process per cpu (e.g. one tornado Python process per CPU)
+    # IOLoop.current().start()
+    IOLoop.instance().start()
+
+
+if __name__ == '__main__':
+    main()
